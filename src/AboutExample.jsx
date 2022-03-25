a554d00d3698e48c98ad20cf941f6026e9ed85b5
Elijah Lopez <elijahllopezz@gmail.com>
2022-03-25 00:09:55 -0400

    Create AboutExample.jsx

---
+++ src/AboutExample.jsx
@@ -0,0 +1,18 @@
+// component example
+import { Text, Anchor, Space  } from '@mantine/core';
+import { useTranslation } from 'react-i18next';
+export default function About() {
+    const { t } = useTranslation();
+    return (
+        <div>
+            <Text>R2-T2 v2022.1 by <Anchor target="_blank" href="//lenerva.com">LeNerva Inc.</Anchor></Text>
+            <Space h="sm" />
+            <Text>Tech: Rust, Tauri, JavaScript, React, Mantine</Text>
+            <Space h="sm" />
+            <Text>{ t('applicableYears') }: 2021</Text>
+            <Space h="md" />
+            <Text>{ t('contactUsMessage') } <Anchor href="mailto:support@lenerva.com?subject=Regarding R2-T2">support@lenerva.com</Anchor></Text>
+        </div>
+        // TODO: FAQ search box
+    );
+}
