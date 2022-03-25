903be020c8c421c5237a4b3c36dd51f6ccfb0a0c
Elijah Lopez <elijahllopezz@gmail.com>
2022-03-24 23:58:26 -0400

    Update and rename index.js to main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// for internationalization (translations)
+import './i18n';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
