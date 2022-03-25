58a88a08c2c74ba26b8346a232c1aaacb1574056
Elijah Lopez <elijahllopezz@gmail.com>
2022-03-24 22:59:36 -0400

    Create i18n.js

---
+++ i18n.js
@@ -0,0 +1,44 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+// defaultLng and translation are used by App.js
+// uncomment and move the following lines to index.js
+// // for internationalization (translations)
+// import './i18n';
+export const defaultLng = 'en';
+export const translations = {
+  en: {
+    translations: {
+      contactUsMessage: 'If you have any questions or requests, feel free to send an email to',
+    }
+  },
+  fr: {
+    translations: {
+      Home: 'Accueil',
+      contactUsMessage: "Si vous avez des questions ou des demandes, n'hésitez pas à envoyer un email à",
+      'Monday to Saturday': 'Lundi à Samedi'
+    }
+  }
+}
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    // we init with resources
+    resources: translations,
+    fallbackLng: defaultLng,
+    debug: false,
+
+    // have a common namespace used around the full app
+    ns: ['translations'],
+    defaultNS: 'translations',
+
+    keySeparator: false, // we use content as keys
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
