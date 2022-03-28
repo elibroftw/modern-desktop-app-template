fd8e83f558c37ddd3959a4556aa9f6a7e35548ce
Elijah Lopez <elijahllopezz@gmail.com>
2022-03-27 20:29:23 -0400

    Update ViewExample.jsx

--- src/ViewExample.jsx
+++ src/ViewExample.jsx
@@ -4,7 +4,8 @@ import { useTranslation } from 'react-i18next';
 export default function About() {
     const { t } = useTranslation();
     return (
-        <div>
+        // this is an alias for <React.Fragment>
+        <>
             <Text>R2-T2 v2022.1 by <Anchor target="_blank" href="//lenerva.com">LeNerva Inc.</Anchor></Text>
             <Space h="sm" />
             <Text>Tech: Rust, Tauri, JavaScript, React, Mantine</Text>
@@ -12,7 +13,7 @@ export default function About() {
             <Text>{ t('applicableYears') }: 2021</Text>
             <Space h="md" />
             <Text>{ t('contactUsMessage') } <Anchor href="mailto:support@lenerva.com?subject=Regarding R2-T2">support@lenerva.com</Anchor></Text>
-        </div>
+        </>
         // TODO: FAQ search box
     );
 }
