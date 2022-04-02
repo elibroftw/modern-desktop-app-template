65765e7e700743dd6dbf58858c28b34fc9d85c94
Elijah Lopez <elijahllopezz@gmail.com>
2022-04-02 16:23:08 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+// boilerplate Mantine providers
+import Providers from './Providers';
 // for internationalization (translations)
 import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>,
   document.getElementById('root')
 );
