be7fc209c0ec8465eab40752e81c4c632a0e5a1a
Elijah Lopez <elijahllopezz@gmail.com>
2022-04-02 16:21:40 -0400

    Create Splashscreen.jsx

---
+++ src/Splashscreen.jsx
@@ -0,0 +1,5 @@
+import { Center, Loader } from '@mantine/core';
+export default function() {
+    // filler
+    return <Center style={{ height: '100vh', width: '100vw' }}><Loader size="xl" /></Center>
+}
