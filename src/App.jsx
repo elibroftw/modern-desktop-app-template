b338cbb1355db663c9876684dd473e6caecf7353
Elijah Lopez <elijahllopezz@gmail.com>
2022-05-16 14:02:59 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import { useLocalForage } from './utils';
 import { translations } from './i18n';
 
 // fallback for React Suspense
-import Fallback from './Fallback';
+import Fallback from './Views/Fallback';
 
 // imported views need to be added to `views`
 // import Home from './Views/Home';
