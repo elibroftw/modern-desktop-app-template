b9abbd07dde999c35e5457ad984661025da59c90
Elijah Lopez <elijahllopezz@gmail.com>
2022-11-22 13:54:39 -0500

    Add rollup visualizer

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import topLevelAwait from 'vite-plugin-top-level-await';
-
+import { visualizer } from 'rollup-plugin-visualizer';
 // https://vitejs.dev/config/
 // https://tauri.app/v1/guides/getting-started/setup/vite#create-the-frontend
 export default defineConfig({
@@ -10,7 +10,8 @@ export default defineConfig({
     topLevelAwait({
       promiseExportName: '__TLA',
       promiseImportName: i => `__TLA_${i}`
-    })
+    }),
+    visualizer()
   ],
   // prevent vite from obscuring rust errors
   clearScreen: false,
