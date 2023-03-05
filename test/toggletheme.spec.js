b5a244e800d68d9656adc35b47ff8d26dec0963b
Elijah Lopez <elijahllopezz@gmail.com>
2023-03-04 20:11:25 -0500

    Update toggletheme.spec.js

--- test/toggletheme.spec.js
+++ test/toggletheme.spec.js
@@ -7,8 +7,7 @@ describe('Home', () => {
 
   it('allows toggling the theme', async () => {
     let bgBeforeToggle = await driver.findElement(By.css('.mantine-Header-root')).getCssValue('background-color');
-    await driver.findElement(By.css('.mantine-UnstyledButton-root > svg')).click();
-    await driver.findElement(By.css("#toggle-theme > svg")).click()
+    await driver.findElement(By.css('#toggle-theme')).click();
     expect(await driver.findElement(By.css('.mantine-Header-root')).getCssValue('background-color')).not.equal(bgBeforeToggle);
   }).timeout(10_000);
 });
