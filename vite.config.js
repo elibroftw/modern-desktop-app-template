41e86b60cd2b04778515b687339a5b42909c842a
Elijah Lopez <elijahllopezz@gmail.com>
2023-05-01 00:48:48 -0400

    Allow developers to remove console from production

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // import topLevelAwait from 'vite-plugin-top-level-await';
 import { visualizer } from 'rollup-plugin-visualizer';
+import removeConsole from 'vite-plugin-remove-console';
 // https://vitejs.dev/config/
 // https://tauri.app/v1/guides/getting-started/setup/vite#create-the-frontend
 export default defineConfig({
@@ -11,7 +12,8 @@ export default defineConfig({
     //   promiseExportName: '__TLA',
     //   promiseImportName: i => `__TLA_${i}`
     // }),
-    visualizer()
+    visualizer(),
+    // removeConsole({includes: ['log', 'assert', 'info', 'error']})
   ],
   // prevent vite from obscuring rust errors
   clearScreen: false,
