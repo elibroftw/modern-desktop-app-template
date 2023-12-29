a7e724bf00948be525157d0676b9ffece2443978
Elijah Lopez <elijahllopezz@gmail.com>
2023-12-29 01:54:05 -0500

    Fix ScrollToTop

--- src/Components/ScrollToTop.jsx
+++ src/Components/ScrollToTop.jsx
@@ -1,9 +1,10 @@
-import { ActionIcon, Affix, Transition } from '@mantine/core';
+import { ActionIcon, Affix, Transition, useMantineTheme } from '@mantine/core';
 import { useEffect, useState } from 'react';
 import { IoArrowUp } from 'react-icons/io5';
 
 export function ScrollToTop({ scroller, bottom = 10 }) {
   const [scrollY, setScrollY] = useState();
+  const theme = useMantineTheme();
 
   const handleScroll = () => {
     setScrollY(scroller.scrollTop);
@@ -22,7 +23,7 @@ export function ScrollToTop({ scroller, bottom = 10 }) {
     <Transition transition='slide-up' mounted={scrollY > 0}>
       {transitionStyles =>
         <div style={transitionStyles} >
-          <ActionIcon size='lg' color='primary' variant='filled'
+          <ActionIcon size='lg' color={theme.primaryColor} variant='filled'
             onClick={() => scroller.scrollTo({ top: 0, behavior: 'smooth' })}>
             <IoArrowUp size={25} />
           </ActionIcon>
