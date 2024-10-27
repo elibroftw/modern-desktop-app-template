a1cf16c7ffd726bc57115142430aff7772143c9a
Elijah Lopez <elijahllopezz@gmail.com>
2024-10-27 18:04:45 -0400

    Improve ScrollToTop

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,17 +1,17 @@
-import { ActionIcon, Affix, Transition, useMantineTheme } from '@mantine/core';
+import { ActionIcon, Affix, Transition, useComputedColorScheme } from '@mantine/core';
 import { useEffect, useState } from 'react';
 import { IoArrowUp } from 'react-icons/io5';
 
 export function ScrollToTop({ scroller, bottom = 10 }) {
   const [scrollY, setScrollY] = useState();
-  const theme = useMantineTheme();
+  const colorScheme = useComputedColorScheme();
 
   const handleScroll = () => {
     setScrollY(scroller.scrollTop);
   };
 
   useEffect(() => {
-    if (scroller !== undefined) {
+    if (scroller !== null) {
       scroller.addEventListener('scroll', handleScroll, { passive: true });
       return () => {
         scroller.removeEventListener('scroll', handleScroll);
@@ -22,12 +22,11 @@ export function ScrollToTop({ scroller, bottom = 10 }) {
   return <Affix position={{ bottom, right: 20 }}>
     <Transition transition='slide-up' mounted={scrollY > 0}>
       {transitionStyles =>
-        <div style={transitionStyles} >
-          <ActionIcon size='lg' color={theme.primaryColor} variant='filled'
-            onClick={() => scroller.scrollTo({ top: 0, behavior: 'smooth' })}>
-            <IoArrowUp size={25} />
-          </ActionIcon>
-        </div>}
+        <ActionIcon style={transitionStyles} size='lg' variant={colorScheme === 'dark' ? 'light' : 'filled'}
+          onClick={() => scroller.scrollTo({ top: 0, behavior: 'smooth' })}>
+          <IoArrowUp size={25} />
+        </ActionIcon>
+      }
     </Transition>
   </Affix>;
 }
