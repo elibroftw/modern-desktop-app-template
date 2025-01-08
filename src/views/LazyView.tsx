30d0846f02bd9f400c2dc8e17a04e1f5f832a5af
Elijah Lopez <elijahllopezz@gmail.com>
2025-01-07 23:03:28 -0500

    fix: lazy component text

--- src/views/LazyView.tsx
+++ src/views/LazyView.tsx
@@ -2,6 +2,6 @@ import { Text } from '@mantine/core';
 
 export default function LazyView() {
 	return (
-		<Text>you should see a loading animation only once</Text>
-	)
+		<Text>you should see a loading animation (if your CPU is slow) or a network request for this component only once</Text>
+	);
 }
